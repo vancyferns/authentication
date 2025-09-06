3b70323743857bb37f96e767cd7832a18de42c25
vancyferns <135420254+vancyferns@users.noreply.github.com>
2025-09-06 04:19:59 +0000

    clerk inetgration works

--- authen/src/app/page.tsx
+++ authen/src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
-import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
+
 export default function Home() {
-  const user=useUser()
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>; // while Clerk is fetching user data
+  }
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {user?<>user?.fullName</>:<>hello</>}
+      {isSignedIn ? (
+        <>{user?.primaryEmailAddress?.emailAddress}</>
+      ) : (
+        <>hello</>
+      )}
     </div>
   );
 }
